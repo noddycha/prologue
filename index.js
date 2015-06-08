497e84a801f6067bff01855fbaa860654e28967e
rajkumarvivre <rajkumar.vie@gmail.com>
2015-06-08 14:13:55 +0530

    Adding file writer

--- index.js
+++ index.js
@@ -3,17 +3,37 @@
 * Testing with existing sample
 */
 
-module.exports = {
-  escape: function(html) {
+(function() {
+
+  function getTimeDifference(currentTimestamp, previousTimestamp) {
+    return currentTimestamp - previousTimestamp;
+  }
+
+  function log(fileName, tagName, message, currentTimestamp, previousTimestamp) {
+    var fs = require('fs');
+    var newLine = '\n';
+    var tab = '\t';
+    var timeDifference = getTimeDifference(currentTimestamp, previousTimestamp);
+    var writeString = tagName + tab + message + tab + currentTimestamp + tab + previousTimestamp + tab + timeDifference + newLine;
+    fs.appendFile(fileName, writeString, function(err) {
+      if(err) {
+        return console.log(err);
+      }
+
+      console.log('File writtern');
+    });
+  }
+
+  function escape(html) {
     return String(html)
       .replace(/&/g, '&amp;')
       .replace(/"/g, '&quot;')
       .replace(/'/g, '&#39;')
       .replace(/</g, '&lt;')
       .replace(/>/g, '&gt;');
-  },
+  }
 
-  unescape: function(html) {
+  function unescape(html) {
     return String(html)
       .replace(/&amp;/g, '&')
       .replace(/&quot;/g, '"')
@@ -21,4 +41,10 @@ module.exports = {
       .replace(/&lt;/g, '<')
       .replace(/&gt;/g, '>');
   }
-};
+
+  module.exports = {
+    log: log,
+    escape: escape,
+    unescape: unescape
+  };
+}).call(this);
